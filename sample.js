f27bc29c673559026a52c2596b9d8b02135de64f
enochcheung <toxicf@gmail.com>
2015-04-07 03:46:49 -0400

    added presentation

--- sample.js
+++ sample.js
@@ -34,9 +34,18 @@ function on_comment(input) {
 
 {"num":0}
 
+
 function on_mention(input) {
-    return on_post(input);
+    var post_content = input.post.content;
+    var post_id = input.post.id;
+    var data = input.data;
+    data.num = data.num+1;
+    
+    var output = {comments:[{post_id:post_id, content:"response to mention: "+post_content+" ("+data.num+")"}], data:data};
+    return output;
 }
+
+
 
 function on_follow(input) {
     var follower = input.follower;
